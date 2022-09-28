f7d07c8ce30997e2be646bb546869d1742f08ca9
JhoanSTalero13 <jhoan.talero@unigermana.edu.co>
2022-09-28 16:56:37 -0500

    subia de prueba

---
+++ src/main/java/com/usa/mintic/g14/EjemploApi/EjemploApiApplication.java
@@ -0,0 +1,13 @@
+package com.usa.mintic.g14.EjemploApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usa/mintic/g14/EjemploApi/EjemploApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usa.mintic.g14.EjemploApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
